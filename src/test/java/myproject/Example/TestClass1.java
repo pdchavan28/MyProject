4cc2b2e6502fe5138729444b2d7208f566c746b3
pdchavan28 <55650815+pdchavan28@users.noreply.github.com>
2021-09-22 15:54:15 +0530

    Update TestClass1.java
    committed directly into master

--- src/test/java/myproject/Example/TestClass1.java
+++ src/test/java/myproject/Example/TestClass1.java
@@ -4,6 +4,8 @@ public class TestClass1 {
 
 	public static void main(String[] args) {
 		System.out.println("Test test");
+		// directly chnages in master
+		System.out.println("Test master chnages");
 
 	}
 
